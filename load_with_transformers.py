0d0aab5742ae2674dd27edb344f0156306e99ccb
marta <ma.moscati@gmail.com>
2024-05-24 07:33:00 +0200

    upload image extractor script

--- load_with_transformers.py
+++ load_with_transformers.py
@@ -5,10 +5,12 @@ from llava.eval.run_llava import load_images
 import glob
 import torch
 import os
+import np
 
 def main():
     model_path = "liuhaotian/llava-v1.5-7b"
     images_path = "/share/hel/datasets/mmimdb/dataset/*.jpeg"
+    encoded_data_path =  "/share/hel/datasets/mmimdb/dataset/llava_encoded_images/"
 
     tokenizer, model, image_processor, context_len = load_pretrained_model(
         model_path=model_path,
@@ -18,22 +20,20 @@ def main():
     del model.model.layers
     del model.lm_head
 
-    print(model)
     image_files = glob.glob(images_path)[:3]
-    names = [os.path.basename(x).split('.')[0] for x in image_files]
+    image_names = [os.path.basename(x).split('.')[0] for x in image_files]
     # print(f'Encoding {one_image_path}...')
     # Not sure if I should follow run_llava line 100 on
     # or model_vqa to encode the image
     images = load_images(image_files)
-    images_tensor = process_images(
+    image_tensors = process_images(
         images,
         image_processor,
         model.config
     ).to(model.device, dtype=torch.float16)
 
-    print(images_tensor[0])
-    print(images_tensor.shape)
-    print(names)
+    feature_file_path = os.path.join(encoded_data_path, 'llava_images.npz')
+    np.savez(feature_file_path, indices=image_names, values=image_tensors)
 
 if __name__ == '__main__':
     main()
