64c34e48ba0d0187eaec6cb777626b58d26a3cd1
mmosc <ma.moscati@gmail.com>
2024-05-27 15:51:53 +0200

    add print for debug

--- llava/model/language_model/llava_llama.py
+++ llava/model/language_model/llava_llama.py
@@ -144,7 +144,7 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
                 image_sizes=image_sizes
             )
         else:
-            print("Does this ever happen?")
+            # print("Does this ever happen?") --> no
             inputs_embeds = self.get_model().embed_tokens(inputs)
 
 
